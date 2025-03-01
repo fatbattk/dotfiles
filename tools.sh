a17f5ab4f74279bda58fa983eb6f3d5b9a1fbda4
FatBat <imfatbat@gmail.com>
2025-03-01 13:34:40 -0800

    Add fzf tool and modify to mass-install tools.

--- tools.sh
+++ tools.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-# Generic package installer function
+# Generic package installer function.
 install_package() {
     local package_name=$1
     
@@ -26,21 +26,47 @@ install_package() {
     fi
 }
 
-# Check and install tools.
+# Check for missing tools.
+missing_special_installs=()
+missing_packages=()
 if ! command_exists eza; then
-    # https://github.com/eza-community/eza/blob/main/INSTALL.md
-    if [[ "$ZSH_HOST_OS" == "linux" ]]; then
-        if ! command_exists gpg; then
-            sudo apt update
-            sudo apt install -y gpg
-        fi
-        sudo mkdir -p /etc/apt/keyrings
-        wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
-        echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
-        sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+    missing_special_installs+=("eza")
+fi
+
+if ! command_exists fzf; then
+    # https://github.com/junegunn/fzf?tab=readme-ov-file#installation
+    missing_packages+=("fzf")
+fi
+
+# Install any missing packages.
+if [ ${#missing_packages[@]} -gt 0 ] || [ ${#missing_special_installs[@]} -gt 0 ]; then
+    if [[ "$ZSH_HOST_OS" == "linux" ]] && command_exists apt; then
         sudo apt update
     fi
-    install_package "eza"
+
+    # Handle special installations first.
+    for package in "${missing_special_installs[@]}"; do
+        case $package in
+            "eza")
+                # https://github.com/eza-community/eza/blob/main/INSTALL.md
+                if [[ "$ZSH_HOST_OS" == "linux" ]]; then
+                    if ! command_exists gpg; then
+                        sudo apt install -y gpg
+                    fi
+                    sudo mkdir -p /etc/apt/keyrings
+                    wget -qO- https://raw.githubusercontent.com/eza-community/eza/main/deb.asc | sudo gpg --dearmor -o /etc/apt/keyrings/gierens.gpg
+                    echo "deb [signed-by=/etc/apt/keyrings/gierens.gpg] http://deb.gierens.de stable main" | sudo tee /etc/apt/sources.list.d/gierens.list
+                    sudo chmod 644 /etc/apt/keyrings/gierens.gpg /etc/apt/sources.list.d/gierens.list
+                fi
+                install_package "eza"
+                ;;
+        esac
+    done
+
+    # Install regular packages.
+    for package in "${missing_packages[@]}"; do
+        install_package "$package"
+    done
 fi
 
 return 0
